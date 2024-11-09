2864f1ba606d5b16ebb2711acf728fa01ffa1d54
DanyYap <124513296+DanyYap@users.noreply.github.com>
2024-11-09 14:11:05 +0800

    New_Script

---
+++ Assets/Script/NoError.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoError : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hi There");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
